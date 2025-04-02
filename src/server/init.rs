e885237365af0079fe35ba204ea93c5e852c2f2b
kursyonish <kurslena173@gmail.com>
2025-04-02 11:29:59 +0300

    add  ADRENALIN_RUSH_CLASSIC

--- src/server/init.rs
+++ src/server/init.rs
@@ -41,7 +41,7 @@ pub(crate) fn new_renet_server() -> (RenetServer, NetcodeServerTransport) {
 }
 
 pub(crate) fn init(sprites: Res<Sprites>, mut commands: Commands) {
-    let name = "blue_sqr".to_string();
+    let name = "adrenalin".to_string();
     let Some(sprite) = sprites.0.get(&name) else {
         panic!("Expected sprite '{}' not found in sprites resource", name)
     };
