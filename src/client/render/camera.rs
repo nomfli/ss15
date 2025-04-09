a13a6246c54c8eb9e0559f8da2d33cfa2fe186db
kursyonish <kurslena173@gmail.com>
2025-04-09 18:23:27 +0300

    delete imports

--- src/client/render/camera.rs
+++ src/client/render/camera.rs
@@ -1,10 +1,9 @@
-use crate::shared::components::{PlayerEntity, Speed};
+use crate::shared::components::PlayerEntity;
 use bevy::prelude::*;
 
 pub(crate) fn camera_follow_smooth(
     player: Query<&Transform, With<PlayerEntity>>,
     mut camera: Query<&mut Transform, (With<Camera>, Without<PlayerEntity>)>,
-    time: Res<Time>,
 ) {
     for mut camera_transform in camera.iter_mut() {
         let Ok(player_transform) = player.get_single() else {
