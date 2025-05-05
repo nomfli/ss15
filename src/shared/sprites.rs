ab1957c9665880f769eb087c03d163b60fcad22f
kursyonish <kurslena173@gmail.com>
2025-05-06 00:48:24 +0300

    make render on client

--- src/shared/sprites.rs
+++ src/shared/sprites.rs
@@ -17,7 +17,11 @@ pub struct Sprites(pub HashMap<String, Sprite>);
 #[derive(Debug, Default, Component, Deserialize, Serialize, Clone)]
 pub struct SpriteName(pub String);
 
-pub fn init_sprites(mut sprites: ResMut<Sprites>, asset_server: Res<AssetServer>) {
+pub fn init_sprites(
+    mut sprites: ResMut<Sprites>,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
     let red_sqr = "red_sqr".to_string();
     sprites.0.insert(
         red_sqr,
@@ -32,5 +36,31 @@ pub fn init_sprites(mut sprites: ResMut<Sprites>, asset_server: Res<AssetServer>
     sprites.0.insert(
         adrenalin,
         Sprite::from_image(asset_server.load("./images/adrenalin.png")),
+    );
+
+    let texture_handle: Handle<Image> = asset_server.load("images/tiles.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::new(64, 64), 2, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let simple_wall = "simple_wall".to_string();
+    sprites.0.insert(
+        simple_wall,
+        Sprite::from_atlas_image(
+            texture_handle.clone(),
+            TextureAtlas {
+                layout: texture_atlas_layout.clone(),
+                index: 1,
+            },
+        ),
+    );
+    let simple_floor = "simple_floor".to_string();
+    sprites.0.insert(
+        simple_floor,
+        Sprite::from_atlas_image(
+            texture_handle,
+            TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 0,
+            },
+        ),
     );
 }
