fbe031e3dd435df04ff72cc81c9d9dd91dafd75a
kursyonish <kurslena173@gmail.com>
2025-05-17 01:49:02 +0300

    make logs macros

--- src/shared/utils.rs
+++ src/shared/utils.rs
@@ -1,27 +1,53 @@
-#[macro_export]
-macro_rules! make_log {
-    ($expr:expr, $ctx:expr) => {
-        match $expr {
+use log::{debug, error};
+
+trait Loggable<T> {
+    fn make_log<F>(self, ctx: &str, on_error: F) -> T
+    where
+        F: FnOnce() -> T;
+}
+
+impl<T: std::fmt::Debug, E: std::fmt::Display> Loggable<T> for Result<T, E> {
+    fn make_log<F>(self, ctx: &str, on_error: F) -> T
+    where
+        F: FnOnce() -> T,
+    {
+        match self {
             Ok(val) => {
-                debug!("[{}] Value: {:?} (at {}:{})", $ctx, val, file!(), line!());
+                debug!("[{}] Ok: {:?}", ctx, val);
                 val
             }
             Err(e) => {
-                error!("[{}] Error: {} (at {}:{})", $ctx, e, file!(), line!());
-                return;
+                error!(" [{}] Error: {}", ctx, e);
+                on_error()
             }
         }
-    };
-    ($expr:expr, $ctx:expr, opt) => {
-        match $expr {
+    }
+}
+
+impl<T: std::fmt::Debug> Loggable<T> for Option<T> {
+    fn make_log<F>(self, ctx: &str, on_error: F) -> T
+    where
+        F: FnOnce() -> T,
+    {
+        match self {
             Some(val) => {
-                debug!("[{}] Some: {:?} (at {}:{})", $ctx, val, file!(), line!());
+                debug!(" [{}] Some: {:?}", ctx, val);
                 val
             }
             None => {
-                debug!("[{}] Got None (at {}:{})", $ctx, file!(), line!());
-                return;
+                debug!("[{}] Got None", ctx);
+                on_error()
             }
         }
-    };
+    }
+}
+
+#[macro_export]
+macro_rules! make_log {
+    ($expr:expr, $ctx:expr) => {{
+        let __temp = $expr;
+        Loggable::make_log(__temp, $ctx, || {
+            return;
+        })
+    }};
 }
