a91485d68f5f18d9e5f504e8db45b08ab59f09b9
kursyonish <kurslena173@gmail.com>
2025-05-18 18:38:04 +0300

    fix log macros

--- src/shared/utils.rs
+++ src/shared/utils.rs
@@ -1,42 +1,34 @@
 use log::{debug, error};
 
-trait Loggable<T> {
-    fn make_log<F>(self, ctx: &str, on_error: F) -> T
-    where
-        F: FnOnce() -> T;
+pub trait Loggable<T> {
+    fn make_log(self, ctx: &str) -> Option<T>;
 }
 
 impl<T: std::fmt::Debug, E: std::fmt::Display> Loggable<T> for Result<T, E> {
-    fn make_log<F>(self, ctx: &str, on_error: F) -> T
-    where
-        F: FnOnce() -> T,
-    {
+    fn make_log(self, ctx: &str) -> Option<T> {
         match self {
             Ok(val) => {
                 debug!("[{}] Ok: {:?}", ctx, val);
-                val
+                Some(val)
             }
             Err(e) => {
                 error!(" [{}] Error: {}", ctx, e);
-                on_error()
+                None
             }
         }
     }
 }
 
 impl<T: std::fmt::Debug> Loggable<T> for Option<T> {
-    fn make_log<F>(self, ctx: &str, on_error: F) -> T
-    where
-        F: FnOnce() -> T,
-    {
+    fn make_log(self, ctx: &str) -> Option<T> {
         match self {
             Some(val) => {
                 debug!(" [{}] Some: {:?}", ctx, val);
-                val
+                Some(val)
             }
             None => {
                 debug!("[{}] Got None", ctx);
-                on_error()
+                None
             }
         }
     }
@@ -46,8 +38,6 @@ impl<T: std::fmt::Debug> Loggable<T> for Option<T> {
 macro_rules! make_log {
     ($expr:expr, $ctx:expr) => {{
         let __temp = $expr;
-        Loggable::make_log(__temp, $ctx, || {
-            return;
-        })
+        Loggable::make_log(__temp, $ctx)
     }};
 }
